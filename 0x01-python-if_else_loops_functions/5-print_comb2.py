9c3098f851c4db79175bba96c17fe71a612782a5
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-29 15:34:41 -0800

    Fix error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 100):
+for x in range(0, 99):
     if x < 99:
-        print("{:02d}, ".format(x), end='')
-print("{} ".format(x), end='')
+        print("{:02d}".format(x), end=', ')
+#print("{} ".format(x), end='')
+print(x + 1)
