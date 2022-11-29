fe3f202e8687ec0c61299ca02c81e03f43c084b7
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-29 11:44:21 -0800

    Fix error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/pyhton3
 for x in range(97, 123):
     if x != 113 and x != 101:
         print(f"{x:c}") 
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,2 +1,3 @@
+#!/usr/bin/pyhton3
 for x in range(0, 99):
     print(f"{x:d} = {hex(x)}")
