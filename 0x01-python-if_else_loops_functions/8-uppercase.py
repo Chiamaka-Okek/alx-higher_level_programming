8ac38a24e55ddbb9f37130c69af02998e5ad361a
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-29 17:23:22 -0800

    First commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    for x in str:
+        ch = ord(x)
+        if ch >= 97 or ch <= 122:
+            print(chr(ch))
+    print("\n")
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+def print_last_digit(number):
+    if number < 0:
+        last_digit = -(number)
+        last_digit = last_digit % 10
+        return -(last_digit)
+    else:
+        last_digit = number % 10
+        return last_digit
