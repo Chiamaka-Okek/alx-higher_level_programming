f248db09ea9669f38b7ac419871d55a7f5af02a3
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-30 14:11:46 -0800

    Fix errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for x in range(0, 10):
         if x <= 7 and x != y and y > x:
             print("{}{}".format(x, y), end=', ')
         elif x > 7 and x != y and y > x:
-            print("{}{} ".format(x, y), end='')
+            print("{}{} ".format(x, y))
 print()
