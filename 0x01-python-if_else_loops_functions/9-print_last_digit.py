9940230d18014bc9b08dadde36fe898f4ea5cf3b
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-29 17:32:44 -0800

    Fix error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,9 @@ def print_last_digit(number):
     if number < 0:
         last_digit = -(number)
         last_digit = last_digit % 10
-        return -(last_digit)
+        print(last_digit, end='')
+        return(last_digit)
     else:
         last_digit = number % 10
+        print(last_digit, end='')
         return last_digit
