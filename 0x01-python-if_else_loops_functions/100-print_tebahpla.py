7a32e864128b635a2787c0f957b168367c57ad97
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-30 15:26:11 -0800

    First commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for x in range(122, 95, -1):
+for x in range(122, 96, -1):
     if x % 10 % 2 != 0:
         x -= 32
     else:
         x
     print("{:c}".format(x), end='')
-print()
