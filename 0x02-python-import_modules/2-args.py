8e67fb5a6cd483f8b9647c766f83528e919216bb
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-01 13:08:39 -0800

    Fix error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,13 @@
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
-        print((len(argv) - 1), "arguments.")
+        print("{} arguments".format(len(argv) - 1))
     elif len(argv) > 2:
-        print((len(argv) - 1), "arguments:")
+        print("{} arguments:".format(len(argv) - 1))
         for x in range(1, (len(argv))):
             str1 = str(x) + ": " + str(argv[x])
-            print(str1)
+            print("{}".format(str1))
     elif len(argv) == 2:
-        print((len(argv) - 1), "argument:")
-        str2 = str((len(argv) - 1)) + ": " + str(argv[1]) 
-        print(str2)
-print()
+        print("{} argument".format(len(argv) - 1))
+        str2 = str(len(argv) - 1) + ": " + str(argv[1])
+        print("{}".format(str2))
