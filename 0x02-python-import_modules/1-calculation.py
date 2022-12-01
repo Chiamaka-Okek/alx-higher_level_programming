deee7f3232f77932835dec1fb3adafd8ada6a2ce
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-01 09:19:44 -0800

    First commit

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+    print(f"{a:d} + {b:d} = {c:d}")
+    print(f"{a:d} - {b:d} = {d:d}")
+    print(f"{a:d} * {b:d} = {e:d}")
+    print(f"{a:d} / {b:d} = {f:d}")
