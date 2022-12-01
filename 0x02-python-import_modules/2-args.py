3a1c6bcdcd844a319fece0110ff39634d7cd1b81
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-01 11:16:15 -0800

    First commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print((len(argv) - 1), "arguments.")
+    elif len(argv) > 2:
+        print((len(argv) - 1), "arguments:")
+        for x in range(1, (len(argv))):
+            str1 = str(x) + ": " + str(argv[x])
+            print(str1)
+    elif len(argv) == 2:
+        print((len(argv) - 1), "argument:")
+        str2 = str((len(argv) - 1)) + ": " + str(argv[1]) 
+        print(str2)
