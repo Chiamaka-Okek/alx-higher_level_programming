731d392eac6fe60e087edba1c907b9d427bf3d1a
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-05 12:34:42 -0800

    Fix error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    number = len(my_list):
+    number = len(my_list)
     if idx < 0 or idx >= number:
         return my_list
     else:
