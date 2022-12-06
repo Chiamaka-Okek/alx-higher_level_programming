c1c03d5dc79e023dccdc13fb5d34b36cae2636ba
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-06 08:49:23 -0800

    First commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,5 @@ def max_integer(my_list=[]):
     if my_list is []:
         return None
     else:
-        for x in range(len(my_list)):
-            if mylist[x] > my_list[x++]:
-                
-                return x
-            else:
-
+        my_list.sort()
+        return my_list[-1]
