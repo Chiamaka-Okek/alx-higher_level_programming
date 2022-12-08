c8f13d4da9290c969d76fac2ee3f294a05e6414c
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-07 16:48:51 -0800

    Fix error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary == None:
-        return
+        return None
     score = []
     for key, value in a_dictionary.items():
         score.append(value)
         score.sort()
         for x in a_dictionary:
             if a_dictionary[x] == score[-1]:
-                return (x)
+                return x
