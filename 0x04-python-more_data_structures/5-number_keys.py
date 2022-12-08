d1823d4339d7a69d9648a4a0504b1ab6c14da1ae
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-07 16:38:32 -0800

    Fix error

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,7 +3,7 @@ def number_keys(a_dictionary):
     count = 0
     for x in range(len(list(a_dictionary))):
         if x == 0:
-            return
-        else:
-            count = count + x
+            return None
+    else:
+        count = count + x
     return count
