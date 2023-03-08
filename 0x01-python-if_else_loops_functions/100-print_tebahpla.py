3fa4714c557b0f0e7ae9059294f8e4ae042f3b66
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-03-08 12:29:07 -0800

    Fix error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 for x in range(122, 96, -1):
     if x % 10 % 2 != 0:
         x -= 32
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for x in range(97, 123):
-    if x != 113 and x != 101:
-        print("{:c}".format(x), end='')
+for x in range(0, 99):
+        print("{} = {}".format(x, hex(x)))
