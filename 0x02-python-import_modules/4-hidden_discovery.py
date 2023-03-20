58f90265acc916ac407ab992105c6558ba861d5f
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-03-20 08:38:13 -0700

    Fix error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-print(hidden_4.pyc.split())
+if __name__ == "__main__":
+    elements = hidden_4.pyc.split()
+    for x in elements:
+        if x[0] == '_':
+            exit(1)
+        else:
+            print(x)
