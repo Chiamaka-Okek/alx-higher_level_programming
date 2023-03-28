20ecfd514f9b33efbf64a6bd98c0908b23f4d14c
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-03-28 16:54:27 -0700

    Fix error

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,7 +39,7 @@ class Square:
     def my_print(self):
         """ Prints to stdout the square in # character """
         if self.__size == 0:
-            print()
+            print("")
         else:
             for i in range(self.__size):
                 print("#" * self.__size)
