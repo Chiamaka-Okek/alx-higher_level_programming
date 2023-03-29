59cce47d490597f6759f74d7410149ff6d4a8e39
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-03-28 17:07:11 -0700

    Fix error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,9 +63,8 @@ class Square:
         """ This prints the character # in required position. """
         if self.__size == 0:
             print()
-        else:
-            [print("") for i in range(self.__position[1])]
-            for i in range(self.__size):
-                [print(" ", end="") for j in range(self.__position[0])]
-                [print("#", end="") for k in range(self.__size)]
-                print("")
+        [print("") for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            print(""i)
