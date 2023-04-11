5ef221ef291f71ebaeb5fc742a529f1ffbc470d1
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-04-11 15:24:37 -0700

    Pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,7 @@
 """ This determines if an object is an instance of a class
 that inherited directly or indirectly from a class """
 
+
 def inherits_from(obj, a_class):
     """ This functions defines the instance of an objects inheritance """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
