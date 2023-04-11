44e08881e0605ca80f86dc512fcd93fb0a9c68ce
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-04-11 16:13:04 -0700

    Class Inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines a class that validates the parameter value """
+
+
+class BaseGeometry:
+    """ Defines class """
+    def area(self):
+        """ Defines module area without implementation """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Defines the module that validates parameter value """
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
