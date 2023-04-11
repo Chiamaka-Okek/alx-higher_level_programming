556e541b203ddb4c90396e7efa089f826ed471f9
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-04-11 12:50:08 -0700

    List object attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This function returns the list of available attributes
+and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Returns the available attributes and methods """
+    return(dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Introduction to inheritance in Python. BElow is a brief descrition of the files contained in this project
