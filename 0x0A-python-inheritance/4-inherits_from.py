43306e213c8158bac44d66600ad55ca2da6c9598
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-04-11 15:19:47 -0700

    Class Inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This determines if an object is an instance of a class
+that inherited directly or indirectly from a class """
+
+def inherits_from(obj, a_class):
+    """ This functions defines the instance of an objects inheritance """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return(True)
+    else:
+        return(False)
